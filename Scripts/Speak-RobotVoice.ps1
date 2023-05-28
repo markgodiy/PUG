54b0a2a08ddf70e9e783f83a9c84841778d72f7a
Melcior <101022486+markgodiy@users.noreply.github.com>
2023-05-28 12:40:30 -0400

    Update Scripts/Speak-RobotVoice.ps1

--- Scripts/Speak-RobotVoice.ps1
+++ Scripts/Speak-RobotVoice.ps1
@@ -1,12 +1,34 @@
 function Speak-RobotVoice {
     param(
-    $phrase
+        $Text,
+        $Gender
     )
-    Add-Type -AssemblyName System.Speech
-    $SpeechSynth = New-Object System.Speech.Synthesis.SpeechSynthesizer
-    $Phrase = Read-Host "What do you want the robot to say?"
-    $SpeechSynth.Speak($phrase)
-            
-    }
+
+Switch ($Gender) {
+    Male {$voiceGender = [System.Speech.Synthesis.VoiceGender]::Male}
+    Female {$voiceGender = [System.Speech.Synthesis.VoiceGender]::Female}
+    Default {$voiceGender = [System.Speech.Synthesis.VoiceGender]::Male}
+}
+
+Add-Type -AssemblyName System.Speech
+
+# Create an instance of SpeechSynthesizer
+$synthesizer = New-Object System.Speech.Synthesis.SpeechSynthesizer
+
+# Get the installed voices
+$voices = $synthesizer.GetInstalledVoices()
+
+# Find the voice with the desired gender
+
+$voice = $voices | Where-Object { $_.VoiceInfo.Gender -eq $desiredGender }
+
+# Set the voice of the synthesizer
+$synthesizer.SelectVoice($voice.VoiceInfo.Name)
+
+# Speak the text with the specified voice
+$text = "Hello, I am a female voice."
+$synthesizer.Speak($text)
+
+}
     
-    Speak-RobotVoice
+Speak-RobotVoice
