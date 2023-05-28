cc389cef5a3916992b8e092888903ea0c6475f23
Melcior <101022486+markgodiy@users.noreply.github.com>
2023-05-28 12:44:28 -0400

    Update Scripts/Speak-RobotVoice.ps1

--- Scripts/Speak-RobotVoice.ps1
+++ Scripts/Speak-RobotVoice.ps1
@@ -1,34 +1,35 @@
 function Speak-RobotVoice {
     param(
-        $Text,
-        $Gender
+        [Parameter(Mandatory = $true)]$Text,
+        [Parameter(Mandatory = $true)]$Gender
     )
 
-Switch ($Gender) {
-    Male {$voiceGender = [System.Speech.Synthesis.VoiceGender]::Male}
-    Female {$voiceGender = [System.Speech.Synthesis.VoiceGender]::Female}
-    Default {$voiceGender = [System.Speech.Synthesis.VoiceGender]::Male}
+
+    #Load Assembly for Spech Synthesizer
+    Add-Type -AssemblyName System.Speech
+
+    # Create an instance of SpeechSynthesizer
+    $synthesizer = New-Object System.Speech.Synthesis.SpeechSynthesizer
+
+    # Define variable based on Gender input
+    Switch ($Gender) {
+        Male { $voiceGender = [System.Speech.Synthesis.VoiceGender]::Male }
+        Female { $voiceGender = [System.Speech.Synthesis.VoiceGender]::Female }
+        Default { $voiceGender = [System.Speech.Synthesis.VoiceGender]::Female }
+    }
+
+    # Get the installed voices
+    $voices = $synthesizer.GetInstalledVoices()
+
+    # Find the voice with the desired gender
+    $voice = $voices | Where-Object { $_.VoiceInfo.Gender -eq $voiceGender }
+
+    # Set the voice of the synthesizer
+    $synthesizer.SelectVoice($voice.VoiceInfo.Name)
+
+    # Speak the text with the specified voice
+    $synthesizer.Speak($text)
+
 }
 
-Add-Type -AssemblyName System.Speech
-
-# Create an instance of SpeechSynthesizer
-$synthesizer = New-Object System.Speech.Synthesis.SpeechSynthesizer
-
-# Get the installed voices
-$voices = $synthesizer.GetInstalledVoices()
-
-# Find the voice with the desired gender
-
-$voice = $voices | Where-Object { $_.VoiceInfo.Gender -eq $desiredGender }
-
-# Set the voice of the synthesizer
-$synthesizer.SelectVoice($voice.VoiceInfo.Name)
-
-# Speak the text with the specified voice
-$text = "Hello, I am a female voice."
-$synthesizer.Speak($text)
-
-}
-    
 Speak-RobotVoice
