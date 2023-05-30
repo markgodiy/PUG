5f9d38d6b1aeef1113586b70860b33b54f7c3fb4
Melcior <101022486+markgodiy@users.noreply.github.com>
2023-05-30 08:32:00 -0400

    Create Get-quser.ps1

---
+++ Scripts/Get-quser.ps1
@@ -0,0 +1,54 @@
+Function Get-quser {
+
+<#
+.DESCRIPTION
+Author: Mark Go
+Object-Oriented version of CMD quser.exe 
+
+.EXAMPLE
+Get-quser <hostname>
+#>
+
+    param (
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true)][Array]$ComputerName
+    )
+   
+    $UserList = @()
+    $sessions = quser /server:$ComputerName | Select-Object -Skip 1
+    
+    foreach ($line in $sessions) {
+           
+        $sessionUsr = ($line -split ' +')[1]
+        $tryGetID = ($line -split ' +')[2]
+        $tryGetID
+
+        if ($tryGetID -match "[a-z]") {
+            $sessionName = ($line -split ' +')[2]
+            $sessionIds = ($line -split ' +')[3]
+            $sessionState = ($line -split ' +')[4]
+            $sessionIdle = ($line -split ' +')[5]
+            $sessionLogon = ($line -split ' +')[6]
+        }
+        else {
+            $sessionIds = $tryGetID
+            $sessionState = ($line -split ' +')[3]
+            $sessionIdle = ($line -split ' +')[4]
+            $sessionLogon = ($line -split ' +')[5]
+        }
+        $obj = New-Object -TypeName PSobject
+        $objRowInfo = [Ordered]@{
+            ID          = $sessionIds
+            
+            USER        = $sessionUsr
+            SESSIONNAME = $sessionName
+            STATE       = $sessionState
+            IDLETIME    = $sessionIdle
+            LOGON       = $sessionLogon
+        }
+        $obj | Add-Member -NotePropertyMembers $objRowInfo -Force
+        $UserList = $UserList + $obj
+    }
+    Return $UserList
+}
+
+
